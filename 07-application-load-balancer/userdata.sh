b4947d41ff7051a7230b9720bf7682b4f754de02
watess01 <stephen.wates@uicentric.com>
2023-10-28 17:21:42 +0100

    2 instance Web server, NLB/ALB

--- 07-application-load-balancer/userdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo su
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-echo 'Web Page for Instance' >> /var/www/html/index.html
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart sshd
